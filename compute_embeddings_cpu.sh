f5128559dd044eaee40f914516e18b71c4ba242a
hamzagamouh <46296228+hamzagamouh@users.noreply.github.com>
2022-11-20 13:40:59 +0100

    Add files via upload

---
+++ compute_embeddings_cpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=debug-long        # partition you want to run job in
+#SBATCH --time=7-00:00:00         # walltime for the job in format (days-)hours:minutes:seconds
+#SBATCH --mail-user=hamza.gamouh@gmail.com --mail-type=END,FAIL     # send email when job changes state to email address 
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == "--"* ]]; then
+        v="${1/--/}"
+        declare "$v"="$2"
+        shift
+    fi
+    shift
+done
+
+
+srun ch-run --bind /home/gamouhh/files:/app/output biopython -- python /app/compute_protein_embeddings.py --emb_name $emb_name --input_dataset $input_dataset --output_folder $output_folder
+
---
+++ compute_embeddings_gpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=gpu-long        # partition you want to run job in
+#SBATCH --gpus=3
+#SBATCH --time=7-00:00:00         # walltime for the job in format (days-)hours:minutes:seconds
+#SBATCH --mail-user=hamza.gamouh@gmail.com --mail-type=END,FAIL     # send email when job changes state to email address 
+
+
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == "--"* ]]; then
+        v="${1/--/}"
+        declare "$v"="$2"
+        shift
+    fi
+    shift
+done
+
+
+srun ch-run --bind /home/gamouhh/files:/app/output biopython -- python /app/compute_protein_embeddings.py --emb_name $emb_name --input_dataset $input_dataset --output_folder $output_folder
+

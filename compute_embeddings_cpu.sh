9e2d69ef7b2c4351a7bfeddfff0b06013d86a2dd
hamzagamouh <46296228+hamzagamouh@users.noreply.github.com>
2022-11-20 11:31:45 +0100

    Update compute_embeddings_cpu.sh

--- compute_embeddings_cpu.sh
+++ compute_embeddings_cpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=debug-long        # partition you want to run job in
 #SBATCH --time=7-00:00:00         # walltime for the job in format (days-)hours:minutes:seconds
-#SBATCH --mail-user=hamza.gamouh@gmail.com --mail-type=END,FAIL     # send email when job changes state to email address 
+#SBATCH --mail-user=your-email --mail-type=END,FAIL     # send email when job changes state to email address (Optional)
 
-srun ch-run biopython python /app/compute_protein_embeddings.py --bind /home/gamouhh/files/MIonSite_embeddings:/app/output
+srun ch-run biopython python /app/compute_protein_embeddings.py --bind /home/username/folder:/app/output 
+
 

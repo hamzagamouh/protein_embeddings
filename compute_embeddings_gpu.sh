66db81afc875c747d47481db8111f674d6ee43c8
hamzagamouh <46296228+hamzagamouh@users.noreply.github.com>
2022-11-20 12:32:50 +0100

    Update compute_embeddings_gpu.sh

--- compute_embeddings_gpu.sh
+++ compute_embeddings_gpu.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=gpu-long        # partition you want to run job in
-#SBATCH --gpus=3                    # Number of GPUs that you want to use (max is 3)
-#SBATCH --time=7-00:00:00         # walltime for the job in format (days-)hours:minutes:seconds 
-#SBATCH --mail-user=your-email   --mail-type=END,FAIL     # send email when job changes state to email address (Optional)
+#SBATCH --gpus=3
+#SBATCH --time=7-00:00:00         # walltime for the job in format (days-)hours:minutes:seconds
+#SBATCH --mail-user=hamza.gamouh@gmail.com --mail-type=END,FAIL     # send email when job changes state to email address 
 
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 
-srun ch-run biopython python /app/compute_protein_embeddings.py --bind /home/username/folder:/app/output 
+srun ch-run --bind /home/gamouhh/files:/app/output biopython -- python /app/compute_protein_embeddings.py --emb_name "emb_name" --input_fasta "filename" --output_folder "output_folder"
+
+

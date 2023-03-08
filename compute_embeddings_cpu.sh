b47c9b84a0412c37bd404c9c6053adcf4e354003
hamzagamouh <46296228+hamzagamouh@users.noreply.github.com>
2023-03-08 10:35:15 +0100

    Update compute_embeddings_cpu.sh

--- compute_embeddings_cpu.sh
+++ compute_embeddings_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=debug-long        # partition you want to run job in
 #SBATCH --time=7-00:00:00         # walltime for the job in format (days-)hours:minutes:seconds
-#SBATCH --mail-user=hamza.gamouh@gmail.com --mail-type=END,FAIL     # send email when job changes state to email address 
+#SBATCH --mail-user=youremail --mail-type=END,FAIL     # send email when job changes state to email address 
 
 while [ $# -gt 0 ]; do
     if [[ $1 == "--"* ]]; then
